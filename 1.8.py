e850151b1a092b63e51131dfc15b2039da5571b2
lizhengxiang <1065482100@qq.com>
2016-01-04 22:12:40 +0800

    python 基础

--- 1.8.py
+++ 1.8.py
@@ -3,3 +3,10 @@ print sqrt(9)
 import cmath
 print cmath.sqrt(-1)
 print (1+8j)*(2+7j)
+#name = raw_input("what your name?")
+#print "hello," + name +"!"
+print 80000l
+print repr(80000l)
+
+temp = 67;
+print "lizhengxinag" + repr(temp)
