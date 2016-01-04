3a15499645aefdfbf216f22c738b80414b0d57d9
lizhengxiang <1065482100@qq.com>
2016-01-04 21:41:24 +0800

    python 基础

---
+++ 1.6.py
@@ -0,0 +1,6 @@
+#coding=utf-8合并字符串
+pieces = ['li', 'zheng', 'xiang']
+piec = '-'.join(pieces)
+
+print 'I am %s' % (piec)
+
---
+++ 1.8.py
@@ -0,0 +1,5 @@
+from math import sqrt
+print sqrt(9)
+import cmath
+print cmath.sqrt(-1)
+print (1+8j)*(2+7j)
