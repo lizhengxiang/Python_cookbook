f805241450ecaa86771dc63a448e9f28d19f1005
lizhengxiang <1065482100@qq.com>
2016-01-06 21:17:51 +0800

    分片

---
+++ 2.2.2.py
@@ -0,0 +1,10 @@
+tag = "www.shejiao520.com"
+print tag[5:12]
+number = [1,2,3,4,5,6,7,8,9]
+print number[3:7]
+print number[:]
+print number[1:]
+print number[:3]
+print number[::3]
+print number[::-1]
+print number[::-2]
