d1f1dfd16f83fbef3eb66cd32251c890d876ede9
lizhengxiang <1065482100@qq.com>
2016-01-07 21:23:50 +0800

    长度最大值和最小值

---
+++ 2.2.5.py
@@ -0,0 +1,7 @@
+# coding: UTF-8
+permission = 'lizhengxiang'
+print 'zh' in permission
+print 'zn' in permission
+number = ['1','2','3','4','5','6']
+#这里面有个坑，你不家引号就找不是到
+print raw_input("Enter number:") in number
---
+++ 2.2.6.py
@@ -0,0 +1,4 @@
+number = [12,31,23,5,6,4,6]
+print len(number)
+print min(number)
+print max(number)
